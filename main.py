9ddb51dfafa973397e41a1273ad8245bfb1e977b
abrewer2 <alexbrewer623@gmail.com>
2025-07-10 20:27:11 -0400

    added read in function

---
+++ main.py
@@ -0,0 +1,12 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read() 
+    print(file_contents)
+    return file_contents
+
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+
+main()
