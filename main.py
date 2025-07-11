0ff4e2f5e483b7ac39a5158614aa609b3b5d385f
abrewer2 <alexbrewer623@gmail.com>
2025-07-10 20:43:57 -0400

    Added stats file and moved function split_test to new file

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
+from stats import split_text
+
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read() 
     return file_contents
-
-def split_text(text):
-    split_list= text.split()
-    word_count=len(split_list)
-    print(f"{word_count} words found in the document")
-    return word_count
-
 
 
 def main():
---
+++ stats.py
@@ -0,0 +1,5 @@
+def split_text(text):
+    split_list= text.split()
+    word_count=len(split_list)
+    print(f"{word_count} words found in the document")
+    return word_count
