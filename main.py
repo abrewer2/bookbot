23ab1cce791714340f6705ea7121b3623451bad4
abrewer2 <alexbrewer623@gmail.com>
2025-07-23 19:48:48 -0400

    added command line text input and added comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import word_count, char_count, sort_on_letters
+import sys
 
-
+#Reading in the the .txt file and returing as a string
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read() 
@@ -8,16 +9,25 @@ def get_book_text(path_to_file):
 
 
 def main():
-    book_text=get_book_text("books/frankenstein.txt")
-    total_word_count=word_count(book_text)
-    word_dic=char_count(book_text)
-    sorted_dic = sort_on_letters(word_dic)
-    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
-    print(f"----------- Word Count ----------\nFound {total_word_count} total words")
-    print("--------- Character Count -------")
-    for dic in sorted_dic:
-        if dic["char"].isalpha():
-            print(f"{dic["char"]}: {dic["num"]}")
+    #Testing to make sure the command include the file path to the book
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        #Reading in file path and sending to get_book_text
+        book_text=get_book_text(sys.argv[1])
+        #Calling word_count to get the total word count
+        total_word_count=word_count(book_text)
+        #Calling char_count to get the a dictionary of all the words in the text
+        word_dic=char_count(book_text)
+        #Calling sort_on_letters to get dictionary ex. {"char": "b", "num": 4868}
+        sorted_dic = sort_on_letters(word_dic)
+        print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
+        print(f"----------- Word Count ----------\nFound {total_word_count} total words")
+        print("--------- Character Count -------")
+        for dic in sorted_dic:
+            if dic["char"].isalpha():
+                print(f"{dic["char"]}: {dic["num"]}")
 
 
 
--- stats.py
+++ stats.py
@@ -4,25 +4,24 @@ def word_count(text):
     #print(f"{word_count} words found in the document")
     return word_count
 
-
+# returns the characters in with a key: charachter, value: count
 def char_count(text):
     char_list_dic={}
     word_list= text.split()
     for word in word_list:
-        #print(word)
         for char in word:
             char=char.lower()
             if char not in char_list_dic:
                 char_list_dic[char] =1
             else:
                char_list_dic[char] +=1 
-    #print(char_list_dic)
     return char_list_dic
 
 
 def sort_on(items):
     return items["num"]
 
+#Return the sorted character dictionary by highest count
 def sort_on_letters(items):
     sort_list=[]
     for char in items:
@@ -32,5 +31,3 @@ def sort_on_letters(items):
         sort_list.append(temp_dic)
     sort_list.sort(reverse=True, key=sort_on)
     return sort_list
-    
-    #print(items)
